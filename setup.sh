da9c2ba1b56092ad24f3c0ddda42821e1562eb7f
Ruy Diaz <diazruy@gmail.com>
2012-08-08 13:20:21 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,10 +3,5 @@
 ln -s ~/dotfiles/bash_aliases ~/.bash_aliases
 ln -s ~/dotfiles/screenrc ~/.screenrc
 ln -s ~/dotfiles/gitconfig ~/.gitconfig
-<<<<<<< HEAD
 ln -s ~/dotfiles/zshrc ~/.zshrc
 ln -s ~/dotfiles/bash_profile ~/.bash_profile
-
-=======
-ln -s ~/dotfiles/zshrc ~/.zshrc
->>>>>>> 47a3f9e0c013b49c032ecb46e02f0b5683ff08a5
