f3424d3426f429d10e89b80d8f0a7e82a3596bb1
Ruy Diaz <diazruy@gmail.com>
2016-10-22 13:17:28 -0700

    Update tmux configuration and setup

--- setup.sh
+++ setup.sh
@@ -6,3 +6,4 @@ ln -s ~/dotfiles/gitconfig ~/.gitconfig
 ln -s ~/dotfiles/zshrc ~/.zshrc
 ln -s ~/dotfiles/bash_profile ~/.bash_profile
 ln -s ~/dotfiles/jslintrc ~/.jslintrc
+ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
