4f22be333c01dc6725330f1cb41c28ad7cbd3d59
Ruy Diaz <diazruy@gmail.com>
2020-08-25 10:27:38 -0700

    Add psql configuration

--- setup.sh
+++ setup.sh
@@ -7,3 +7,4 @@ ln -s ~/dotfiles/zshrc ~/.zshrc
 ln -s ~/dotfiles/bash_profile ~/.bash_profile
 ln -s ~/dotfiles/jslintrc ~/.jslintrc
 ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/psqlrc ~/.psqlrc
